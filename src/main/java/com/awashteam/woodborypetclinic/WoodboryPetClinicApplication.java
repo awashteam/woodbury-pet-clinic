601ee8bee6a0bcb13b7b4db91385b219ebed3436
awashteam <awashteam@gmail.com>
2020-09-27 15:10:04 -0500

    Initial Project

---
+++ src/main/java/com/awashteam/woodborypetclinic/WoodboryPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.awashteam.woodborypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodboryPetClinicApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WoodboryPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/awashteam/woodborypetclinic/WoodboryPetClinicApplicationTests.java
@@ -0,0 +1,14 @@
+package com.awashteam.woodborypetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class WoodboryPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
