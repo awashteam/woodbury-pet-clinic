291eb20b8928e1fa34fac98a755da4c127220161
awashteam <awashteam@gmail.com>
2020-09-27 16:03:32 -0500

    adding POJOS. Closes #2

---
+++ src/main/java/com/awashteam/woodborypetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.awashteam.woodborypetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/awashteam/woodborypetclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package com.awashteam.woodborypetclinic.model;
+
+public class Vet extends Person{
+
+
+}
